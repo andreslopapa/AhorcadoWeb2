49fd029dad7fcbe0d3888115c60bdbf102068828
Joaquin Sanchez <sanchezjoaquin1995@gmail.com>
2018-08-19 01:04:15 -0300

    Gecko travis

--- src/test/java/com/agiles/UI/selenide/TestUIAhorcado.java
+++ src/test/java/com/agiles/UI/selenide/TestUIAhorcado.java
@@ -113,7 +113,7 @@ public class TestUIAhorcado {
 	public static void openBrowser(){
 		
 		try{
-			System.setProperty("webdriver.gecko.driver","/home/circleci/repo/geckodriver");
+			System.setProperty("webdriver.gecko.driver","/home/travis/geckodriver");
 			
 			//ChromeOptions options = new ChromeOptions();
 			FirefoxOptions options = new FirefoxOptions();
